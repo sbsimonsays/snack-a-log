bac0c8e1a39199495111da18fdcf010df6c23346
Spencer Simon <spencer.san@gmail.com>
2022-08-21 15:03:39 -0400

    Vomit on my sweater, already

--- front-end/src/Components/Snack.js
+++ front-end/src/Components/Snack.js
@@ -8,7 +8,7 @@ function Snack({ snack }) {
       <a href={`/snacks/${snack.id}`}>
         <h4><img src={snack.image} alt={snack.is_healthy ? "healthy food" : "unhealthy food"}></img></h4>
       
-      <h4><img src={ snack.is_healthy ? heartSolid : heartOutline} alt={snack.is_healthy ? "healthy food" : "unhealthy food"} /> {snack.name}</h4></a>
+      <h4><span><img src={ snack.is_healthy ? heartSolid : heartOutline} alt={snack.is_healthy ? "healthy food" : "unhealthy food"} /> {snack.name}</span></h4></a>
     </div>
   );
 }
